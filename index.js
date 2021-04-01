13247e2eeedcab28f8d7a42df699e0af4102a0fc
sunitee <suniteebajpai2@gmail.com>
2021-04-01 11:16:56 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+
+
+const drivers = ["Tom", "Mat", "Don", "Jake", "Craig"]
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('DOM fully loaded and parsed');
+
+    function returnFirstTwoDrivers(drivers) {
+        console.log(drivers.slice(0,2))
+        return drivers.slice(0,2)   
+    }
+
+
+    function returnLastTwoDrivers(drivers){
+        console.log(drivers.slice(-2))
+        return drivers.slice(-2)
+    }
+
+    const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+    
+    const createFareMultiplier = function(multiplyValue){
+
+        return function(value){
+            console.log (multiplyValue * value)
+            return multiplyValue * value
+        }   
+    }
+
+    const fareDoubler = createFareMultiplier(2)
+
+    const fareTripler = createFareMultiplier(3)
+
+    const selectDifferentDrivers = function(drivers,newSet) {
+        return newSet(drivers)
+    }
+
+        
+
+    returnLastTwoDrivers(drivers)
+    returnFirstTwoDrivers(drivers)
+    createFareMultiplier()
+
+
+// });
